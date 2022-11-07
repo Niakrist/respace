9219281ba8448c1e02b9d60e3249dd5f5fdfff85
Niakrist <ak_88dron@inbox.ru>
2022-11-07 13:17:22 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@ const plan = document.querySelector('#plan')
 const changeImg = document.querySelector('#change-img');  
 
 plan.addEventListener('click', function() {
-  changeImg.href = './img/petra-plan.jpg'
+  changeImg.src = './img/petra-plan.jpg'
   console.log('./img/petra-plan.jpg');
 })
 
 fasad.addEventListener('click', function() {
-  changeImg.href = './img/petra-fasad.jpg'
+  changeImg.src = './img/petra-fasad.jpg'
   console.log('./img/petra-fasad.jpg');
 })
