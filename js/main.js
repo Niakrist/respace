741bd881162f8ad1eca0341623c315a89b412aa3
Niakrist <ak88dron@gmail.com>
2022-11-15 11:43:21 +0300

    add adaptive css

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
 const fasad = document.querySelector('#fasad');
 const plan = document.querySelector('#plan')
 const changeImg = document.querySelector('#change-img');  
+const btnMenu = document.querySelector('.btn__menu');
+const navMenuMob = document.querySelector('.nav__menu-mob');
 
-plan.addEventListener('click', function() {
-  changeImg.src = './img/petra-plan.jpg'
-  console.log('./img/petra-plan.jpg');
-})
+// plan.addEventListener('click', function() {
+//   changeImg.src = './img/petra-plan.jpg'
+//   console.log('./img/petra-plan.jpg');
+// })
 
-fasad.addEventListener('click', function() {
-  changeImg.src = './img/petra-fasad.png'
-  console.log('./img/petra-fasad.png');
+// fasad.addEventListener('click', function() {
+//   changeImg.src = './img/petra-fasad.png'
+//   console.log('./img/petra-fasad.png');
+// })
+
+
+btnMenu.addEventListener('click', function() {
+  navMenuMob.classList.toggle('nav__menu-mob--active');
 })
