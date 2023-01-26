0576b5041fefa881cf11808d51dba261d8466fa0
Niakrist <ak88dron@gmail.com>
2023-01-26 15:17:30 +0300

    add js slider

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-const fasad = document.querySelector('#fasad');
-const plan = document.querySelector('#plan')
-const changeImg = document.querySelector('#change-img');  
-const btnMenu = document.querySelector('.btn__menu');
-const navMenuMob = document.querySelector('.nav__menu-mob');
+// const fasad = document.querySelector('#fasad');
+// const plan = document.querySelector('#plan')
+// const changeImg = document.querySelector('#change-img');  
+// const btnMenu = document.querySelector('.btn__menu');
+// const navMenuMob = document.querySelector('.nav__menu-mob');
 
 // plan.addEventListener('click', function() {
 //   changeImg.src = './img/petra-plan.jpg'
@@ -15,6 +15,32 @@ const navMenuMob = document.querySelector('.nav__menu-mob');
 // })
 
 
-btnMenu.addEventListener('click', function() {
-  navMenuMob.classList.toggle('nav__menu-mob--active');
+// btnMenu.addEventListener('click', function() {
+//   navMenuMob.classList.toggle('nav__menu-mob--active');
+// })
+
+/* */
+
+const slider = document.querySelector('#slider');
+const sliderList = document.querySelectorAll('#slider .slide__img');
+console.log(sliderList);
+
+sliderList.forEach(function(slide, index) {
+  slide.dataset.index = index;
+  
+  if (index != 0) slide.classList.add('hidden');
+
+  sliderList[0].setAttribute('data-active', '');
+
+  slide.addEventListener('click', function() {
+    this.classList.add('hidden');
+    this.removeAttribute('data-active');
+
+    const slideNextIndex = Number(slide.dataset.index) + 1 < sliderList.length ? Number(slide.dataset.index) + 1 : 0;
+    console.log(slideNextIndex +' - ' + sliderList.length)
+    const slideNext = slider.querySelector(`[data-index='${slideNextIndex}']`);
+    slideNext.classList.remove('hidden');
+  })
+
+
 })
